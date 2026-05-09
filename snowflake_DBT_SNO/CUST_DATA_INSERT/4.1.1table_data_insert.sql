447889a02e82aa9c2a145c6f14ba558f9f6ccd01
Lokesh <lokeshlucky1989@example.com>
2026-05-09 21:05:16 +0530

    Initial commit: Added project files

---
+++ snowflake_DBT_SNO/CUST_DATA_INSERT/4.1.1table_data_insert.sql
@@ -0,0 +1,31 @@
+-- Insert last 5 customers
+INSERT INTO LOK_CUSTOMERS VALUES
+(21, 'Ajay Malhotra', 'ajay@gmail.com', 'Chandigarh'),
+(22, 'Sunita Roy', 'sunita@gmail.com', 'Guwahati'),
+(23, 'Ramesh Iyer', 'ramesh@gmail.com', 'Madurai'),
+(24, 'Kavita Desai', 'kavita@gmail.com', 'Vadodara'),
+(25, 'Mohit Agarwal', 'mohit@gmail.com', 'Agra');
+
+-- Insert last 5 products
+INSERT INTO LOK_PRODUCTS VALUES
+(121, 'Wireless Mouse', 'Accessories', 1800),
+(122, 'Gaming Keyboard', 'Accessories', 6500),
+(123, 'Office Chair', 'Furniture', 12000),
+(124, 'WiFi Extender', 'Networking', 5200),
+(125, 'Noise Cancelling Headset', 'Audio', 14500);
+
+-- Insert last 5 orders
+INSERT INTO LOK_ORDERS VALUES
+(1021, 21, '2024-01-21', 'DELIVERED'),
+(1022, 22, '2024-01-22', 'PENDING'),
+(1023, 23, '2024-01-23', 'DELIVERED'),
+(1024, 24, '2024-01-24', 'DELIVERED'),
+(1025, 25, '2024-01-25', 'CANCELLED');
+
+-- Insert last 5 order items
+INSERT INTO LOK_ORDER_ITEMS VALUES
+(21, 1021, 121, 2),
+(22, 1022, 122, 1),
+(23, 1023, 123, 1),
+(24, 1024, 124, 1),
+(25, 1025, 125, 1);
---
+++ snowflake_DBT_SNO/README.md
@@ -0,0 +1,2 @@
+# SNO_DBT_SCD_PRO_REPO
+this is practice purpose creating to tarnsform data in DBT and load data in snowflake source and atrget is snowflake
